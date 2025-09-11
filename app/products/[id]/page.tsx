369902dc205862486747cedfe74597e4d7967069
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2025-09-11 19:40:26 +0800

    store-2025/9/11

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import { formatCurrency } from '@/utils/format';
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/single-product/AddToCart';
 import ProductRating from '@/components/single-product/ProductRating';
+import ShareButton from '@/components/single-product/ShareButton';
 
 async function SingleProductPage({
   params,
@@ -38,7 +39,10 @@ async function SingleProductPage({
         <div>
           <div className="flex gap-x-8 items-center">
             <h1 className="capitalize text-3xl font-bold">{name}</h1>
-            <FavoriteToggleButton productId={id} />
+            <div className="flex items-center gap-x-2">
+              <ShareButton name={product.name} productId={id} />
+              <FavoriteToggleButton productId={id} />
+            </div>
           </div>
           <ProductRating productId={id} />
           <h4 className="text-xl mt-2">{company}</h4>
